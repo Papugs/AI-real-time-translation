8772525bb2834f7608762ced5fcdefcf94f54d87
DzwoniPapuga <127849496+DzwoniPapuga@users.noreply.github.com>
2024-05-01 19:58:53 +0100

    Created iOS app project

---
+++ Real-Time-Translator/Real-Time-Translator/Real_Time_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Time_TranslatorApp.swift
+//  Real-Time-Translator
+//
+//  Created by Norbert Papuga on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Real_Time_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Real-Time-Translator/Real-Time-TranslatorUITests/Real_Time_TranslatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Real_Time_TranslatorUITestsLaunchTests.swift
+//  Real-Time-TranslatorUITests
+//
+//  Created by Norbert Papuga on 01/05/2024.
+//
+
+import XCTest
+
+final class Real_Time_TranslatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
